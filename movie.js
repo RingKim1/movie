b70755dd6289568b9d269e31e255a03b4c43b64b
RingKim1 <kgh9393@naver.com>
2024-04-25 20:03:58 +0900

    feat:API link

--- movie.js
+++ movie.js
@@ -1,12 +1,28 @@
 const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmU5YjdlMjE5NDNlZWNiZjEyMzJiMDRhYTVlYzI1OSIsInN1YiI6IjY2MjhkOTc3OTFmMGVhMDE3ZTAxMDU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kocb84g6nnDUdrQpEt7sq5BdKfrIqhJDiHucIzViMb0'
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmU5YjdlMjE5NDNlZWNiZjEyMzJiMDRhYTVlYzI1OSIsInN1YiI6IjY2MjhkOTc3OTFmMGVhMDE3ZTAxMDU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kocb84g6nnDUdrQpEt7sq5BdKfrIqhJDiHucIzViMb0'
     }
-  };
-  
-  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+};
+
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+        console.log(response)
+        for (let i = 0; i < response["results"].length; i++) {
+            let image = response["results"][i]["poster_path"];
+
+            const moviecontainer = document.querySelector('#moviecontainer');
+            
+            let img = document.createElement("img");
+            img.setAttribute("src", `https://image.tmdb.org/t/p/w500${image}`);
+            moviecontainer.appendChild(img);
+        }
+
+    })
+
     .catch(err => console.error(err));
+
+
+
